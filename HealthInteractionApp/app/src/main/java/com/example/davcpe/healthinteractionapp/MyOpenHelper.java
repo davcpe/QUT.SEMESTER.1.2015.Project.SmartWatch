9f1ceac21c82d9c5a8e42bccfeb6ca44334b8ff0
davit sanpote <supawadeewithdavit@gmail.com>
2015-03-17 01:49:27 +1000

    Update 1.8 Failed with new Fragment UI

---
+++ HealthInteractionApp/app/src/main/java/com/example/davcpe/healthinteractionapp/MyOpenHelper.java
@@ -0,0 +1,37 @@
+package com.example.davcpe.healthinteractionapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by davcpe on 3/12/2015.
+ */
+public class MyOpenHelper extends SQLiteOpenHelper {
+
+    private  static final String DATABASE_NAME = "Health.db";
+    private  static  final  int DATABASE_VERSION =1;
+    private static  final String USER_TABLE ="create table userTABLE (_no integer primary key,"+" user_id text, user_name text, user_password text, user_officer text);";
+   // private static  final String CATEGORY_TABLE ="create table categoryTABLE (_no integer primary key,"+" category_id text, category_name text);";
+
+    public MyOpenHelper(Context context){
+
+        super(context, DATABASE_NAME,null,DATABASE_VERSION);
+
+    }//Constructor
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(USER_TABLE);
+        //db.execSQL(CATEGORY_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ HealthInteractionApp/app/src/main/java/com/example/davcpe/healthinteractionapp/UserTABLE.java
@@ -0,0 +1,71 @@
+package com.example.davcpe.healthinteractionapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by davcpe on 3/12/2015.
+ */
+public class UserTABLE {
+
+    private MyOpenHelper objMyOpenHelper;
+    private SQLiteDatabase writeSQLite, readSQLite;
+    public static final String TABLE_USER = "userTABLE";
+    public static final String COLUMN_NO_USER  ="_no";
+    public static final String COLUMN_USER_ID = "user_id";
+    public static final String COLUMN_USER_NAME = "user_name";
+    public static  final String COLUMN_USER_PASSWORD = "user_password";
+    public static  final String COLUMN_OFFICER = "user_officer";
+
+
+
+
+
+    public UserTABLE(Context context) {
+        objMyOpenHelper = new MyOpenHelper(context);
+        writeSQLite = objMyOpenHelper.getWritableDatabase();
+        readSQLite = objMyOpenHelper.getReadableDatabase();
+    }// Constructor
+
+    public String[]searchUser(String strUSerName){
+        try {
+
+            String strData[]= null;
+            Cursor objCursor = readSQLite.query(TABLE_USER,
+                    new String[] {COLUMN_NO_USER, COLUMN_USER_ID,COLUMN_USER_NAME,COLUMN_USER_PASSWORD,COLUMN_OFFICER},COLUMN_USER_NAME+"=?",
+                    new String[] {String.valueOf(strUSerName)}, null,null,null,null);
+            if (objCursor != null) {
+                if(objCursor.moveToFirst()){
+                    strData = new String[objCursor.getColumnCount()];
+                    strData[0] = objCursor.getString(0);
+                    strData[1] = objCursor.getString(1);
+                    strData[2] = objCursor.getString(2);
+                    strData[3] = objCursor.getString(3);
+                    strData[4] = objCursor.getString(4);
+                }// if2
+            }//if1
+
+            objCursor.close();
+            return strData;
+        }catch (Exception e){
+            return null;
+        }
+
+
+    }//search User
+
+    public long addValueUser (String strUserID, String strUserName, String strPassword, String strOfficer){
+        ContentValues objContentValues = new ContentValues();
+
+        objContentValues.put(COLUMN_USER_ID, strUserID);
+        objContentValues.put(COLUMN_USER_NAME, strUserName);
+        objContentValues.put(COLUMN_USER_PASSWORD, strPassword);
+        objContentValues.put(COLUMN_OFFICER, strOfficer);
+        return writeSQLite.insert(TABLE_USER, null,objContentValues);
+    } //addValueToUser
+
+
+
+}
